02df6f3a073feabc17d14e71cda2bdb6d21cb968
Chris Caragianis <chriscaragianis@gmail.com>
2018-11-29 13:32:40 -0500

    0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExSimpleGraph.MixProject do
   def project do
     [
       app: :ex_simple_graph,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
